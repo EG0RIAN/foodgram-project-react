08340f39af1a2a67200c4ee46998cad35f0667ed
Egor <e.arkhiptsev@gmail.com>
2023-06-18 17:10:29 +0300

    pep8 fix

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,6 +1,5 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.hashers import make_password
-from django.db import transaction
 from drf_extra_fields.fields import Base64ImageField
 from rest_framework import serializers
 from rest_framework.validators import UniqueTogetherValidator
